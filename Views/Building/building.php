d2b1a4ea74293c7db3691ec604fc0573c8a882d0
IlmanNafi11 <ilmannafi04@gmail.com>
2024-09-28 17:51:06 +0700

    koding ui menu gedung

---
+++ Views/Building/building.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--    Bootstrap Css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Core UI css -->
+    <link href="https://cdn.jsdelivr.net/npm/@coreui/coreui@5.1.2/dist/css/themes/bootstrap/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-72sGGfjIx6qT6nqLY5JXJdwHV+8GR6BXqIJMnei1+xNtrRVP9GM/vFJ3+9345bt/"
+        crossorigin="anonymous">
+
+    <!--    Custom Css-->
+    <link rel="stylesheet" href="../../Asset/css/style.css">
+
+    <!-- Font -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Saira:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+
+    <title>Gedung | BLK</title>
+</head>
+
+<body>
+
+    <!-- ROOT Container -->
+    <div class="container-fluid main-root-container m-0 p-0 w-100 h-100">
+        <!-- Navbar -->
+        <?php require_once '../Layout/navbar.php' ?>
+        <!-- Container Content -->
+        <div class="container-fluid container-content d-flex flex-column">
+            <!-- Breadcrumb -->
+            <nav aria-label="breadcrumb" class="breadcrumb-content">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="../Dashboard/dashboard.php">Beranda</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Gedung</li>
+                </ol>
+            </nav>
+
+            <?php require_once '../Layout/Tables/buildingTables.php' ?>
+
+            <!-- Noted : tambahkan kondisional untuk melacak jumlah minimum data kemunculan pagination -->
+            <!-- Pagination -->
+            <div class="pagination-container d-flex justify-content-center">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <!--    Bootstrap Js-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+    <!-- Core UI Js -->
+    <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@5.1.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-i+Yu7CmJG/p8FA6Avyg4ZheFvxNjJQ5taj5ArZf94yQt1lWZiVwkXyPrgE/QqbJi"
+        crossorigin="anonymous"></script>
+
+    <!-- Custom Js-->
+    <script src="../../Asset/js/script.js"></script>
+
+</body>
+
+</html>
