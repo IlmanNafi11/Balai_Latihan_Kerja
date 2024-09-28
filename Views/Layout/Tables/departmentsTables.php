2154358be1b5adebf6c7bee9789e02064b2a4d17
IlmanNafi11 <ilmannafi04@gmail.com>
2024-09-28 15:38:02 +0700

    membuat table department

---
+++ Views/Layout/Tables/departmentsTables.php
@@ -0,0 +1,30 @@
+<div class="tables tables-department-container px-0 m-0">
+    <!-- Button add data -->
+    <div class="button-add-data-container">
+        <a href="addDepartments.php"><button class="btn btn-primary">+ Tambah Data</button></a>
+    </div>
+    <!-- Table -->
+    <div class="table-responsive-sm table-responsive-md">
+        <table class="table table-hover align-middle">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Nama</td>
+                    <td>Deskripsi</td>
+                    <td>Aksi</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Tata Busana</td>
+                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</td>
+                    <td>
+                        <a href="updateDepartments.php"><button class="btn btn-warning">Ubah</button></a>
+                        <a href="#"><button class="btn btn-danger">Hapus</button></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
