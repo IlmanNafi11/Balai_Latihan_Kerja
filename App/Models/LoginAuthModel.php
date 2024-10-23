2d7ee81cbbb9b84805be10f128e9ee882785e9f1
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-23 15:02:38 +0700

    coding logika bisnis get account untuk fungsional login

--- App/Models/LoginAuthModel.php
+++ App/Models/LoginAuthModel.php
@@ -3,9 +3,28 @@
 class LoginAuthModel
 {
     private $connection;
-private $api = "121232133";
     function __construct($db)
     {
         $this->connection = $db;
+    }
+
+    public function login($username, $password)
+    {
+        $query = "SELECT * FROM users WHERE email = :email LIMIT 1";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindValue(':email', $username);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            if ($password == $user['password']) {
+                return $user;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
