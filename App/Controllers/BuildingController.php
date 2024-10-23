b5e4dcfca42ad4b32a64a5f41f5ef80e312fe6fe
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-23 21:02:06 +0700

    koding logika insert data pada controller

--- App/Controllers/BuildingController.php
+++ App/Controllers/BuildingController.php
@@ -1,14 +1,16 @@
 <?php
 require_once '../App/Config/Database.php';
-require_once '../App/Models/LoginAuthModel.php';
+require_once '../App/Models/BuildingModel.php';
+
 class BuildingController
 {
     private $model;
+
     public function __construct()
     {
         $database = new Database();
         $db = $database->getConnection();
-
+        $this->model = new BuildingModel($db);
     }
 
     public function index()
@@ -24,5 +26,21 @@ class BuildingController
     public function viewUpdateBuilding()
     {
         require_once '../App/Views/Building/updateBuilding.php';
+    }
+
+    public function creteBuilding()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = json_decode(file_get_contents("php://input"), true);
+            $namaGedung = $data['name'];
+            $deskripsiGedung = $data['description'];
+
+            if (!empty($namaGedung) && !empty($deskripsiGedung)) {
+                $result = $this->model->createBuilding($namaGedung, $deskripsiGedung);
+                echo json_encode($result);
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Data tidak lengkap']);
+            }
+        }
     }
 }
