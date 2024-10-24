28f934e4a31929f8b5d69a6204d87deb8d875d93
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-24 17:49:00 +0700

    rebase

--- Public/Asset/js/buildings/building.js
+++ Public/Asset/js/buildings/building.js
@@ -1,1 +1,13 @@
-
+function deleteBuilding(id){
+    axios.delete(`/building/delete/${id}`)
+        .then(response => {
+            if (response.data.success){
+                window.location.href = response.data.redirect_url;
+            } else {
+                alert(response.data.message);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
