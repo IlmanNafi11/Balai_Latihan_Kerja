59d6e146c445ba099c35ea2d6527326a6e9ec4b1
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-25 18:55:08 +0700

    fix exception handler PDO di model institute dan menyediakan metode untuk get id institusi

--- App/Models/InstituteModel.php
+++ App/Models/InstituteModel.php
@@ -8,31 +8,52 @@ class InstituteModel
         $this->connection = $db;
     }
 
-    public function getAllInstituteData()
+    public function getAllInstitute()
     {
-        $query = "SELECT * FROM institusi";
+        $query = "SELECT * FROM institute";
         $stmt = $this->connection->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($stmt->execute()) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+    public function getInstituteId()
+    {
+        $query = "SELECT id FROM institute";
+        $stmt = $this->connection->prepare($query);
+        if ($stmt->execute()) {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            return ['success' => false, 'message' => 'Gagal mengambil id insitute'];
+        }
     }
 
     public function updateInstitute($data = [])
     {
         $query = "UPDATE institute SET nama = :nama, pimpinan = :pimpinan, no_vin = :no_vin, no_sotk = :no_sotk, thn_berdiri = :thn_berdiri, tipe = :tipe, kepemilikan = :kepemilikan, status_beroperasi = :status_beroperasi, no_tlp = :no_tlp, no_fax = :no_fax, email = :email, website = :website, deskripsi = :deskripsi";
         $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(':nama', $data['nama']);
-        $stmt->bindParam(':pimpinan', $data['pimpinan']);
-        $stmt->bindParam(':no_vin', $data['no_vin']);
-        $stmt->bindParam(':no_sotk', $data['no_sotk']);
-        $stmt->bindParam(':thn_berdiri', $data['thn_berdiri']);
-        $stmt->bindParam(':tipe', $data['tipe']);
-        $stmt->bindParam(':kepemilikan', $data['kepemilikan']);
-        $stmt->bindParam(':status_beroperasi', $data['status_beroperasi']);
-        $stmt->bindParam(':no_tlp', $data['no_tlp']);
-        $stmt->bindParam(':no_fax', $data['no_fax']);
-        $stmt->bindParam(':email', $data['email']);
-        $stmt->bindParam(':website', $data['website']);
-        $stmt->bindParam(':deskripsi', $data['deskripsi']);
-        return $stmt->execute();
+        try {
+            $stmt->bindParam(':nama', $data['nama']);
+            $stmt->bindParam(':pimpinan', $data['pimpinan']);
+            $stmt->bindParam(':no_vin', $data['no_vin']);
+            $stmt->bindParam(':no_sotk', $data['no_sotk']);
+            $stmt->bindParam(':thn_berdiri', $data['thn_berdiri']);
+            $stmt->bindParam(':tipe', $data['tipe']);
+            $stmt->bindParam(':kepemilikan', $data['kepemilikan']);
+            $stmt->bindParam(':status_beroperasi', $data['status_beroperasi']);
+            $stmt->bindParam(':no_tlp', $data['no_tlp']);
+            $stmt->bindParam(':no_fax', $data['no_fax']);
+            $stmt->bindParam(':email', $data['email']);
+            $stmt->bindParam(':website', $data['website']);
+            $stmt->bindParam(':deskripsi', $data['deskripsi']);
+            if ($stmt->execute()) {
+                return ['success' => true, 'message' => 'Data Institusi Berhasil Diperbaharui', 'redirect_url' => '/institute`'];
+            } else
+            {
+                return ['success' => false, 'message' => 'Data Institusi Gagal Diperbaharui'];
+            }
+        } catch (PDOException $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
     }
 }
