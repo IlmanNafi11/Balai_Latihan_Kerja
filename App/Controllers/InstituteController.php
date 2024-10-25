a245937789440d47643d1b5c1b40b4d950dce1d0
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-25 19:00:01 +0700

    menambahkan metode untuk get id institusi

--- App/Controllers/InstituteController.php
+++ App/Controllers/InstituteController.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../App/Config/Database.php';
-require_once '../App/Models/LoginAuthModel.php';
+require_once '../App/Models/InstituteModel.php';
 class InstituteController
 {
     private $model;
@@ -9,11 +9,21 @@ class InstituteController
     {
         $database = new Database();
         $db = $database->getConnection();
-
+        $this->model = new InstituteModel($db);
     }
 
     public function index()
     {
         require_once "../App/Views/Institute/institute.php";
+    }
+
+    public function viewUpdateInstitute()
+    {
+        require_once "../App/Views/Institute/updateInstitute.php";
+    }
+
+    public function getInstituteId()
+    {
+        echo json_encode($this->model->getInstituteId());
     }
 }
