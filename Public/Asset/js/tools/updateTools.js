7ff7b5b5ed4e57ee882302aef04281f0953dc7f5
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-25 08:15:45 +0700

    koding logic request update alat dan exception handler client side

---
+++ Public/Asset/js/tools/updateTools.js
@@ -0,0 +1,172 @@
+const name = document.getElementById('nama-alat');
+const type = document.getElementById('tipe-alat');
+const description = document.getElementById('deskripsi-alat');
+btnSimpan = document.getElementById('btn-simpan');
+validName = name.nextElementSibling;
+invalidName = validName.nextElementSibling;
+validType = type.nextElementSibling;
+invalidType = validType.nextElementSibling;
+validDescription = description.nextElementSibling;
+invalidDescription = validDescription.nextElementSibling;
+
+const regex = /^[a-zA-Z0-9 .,]+$/;
+
+let id = window.location.pathname.split('/').pop();
+
+axios.get(`/tools/getTools/${id}`)
+    .then(response => {
+        if (response.data.success) {
+            name.value = response.data.dataByID.nama;
+            type.value = response.data.dataByID.tipe;
+            description.value = response.data.dataByID.deskripsi;
+        } else if (response.data.success == false) {
+            swalWithBootstrapButtons.fire({
+                title: "Gagal!", text: response.data.message, icon: "error"
+            });
+        }
+    })
+    .catch(error => {
+        swalWithBootstrapButtons.fire({
+            title: "Gagal!", text: error.message, icon: "error"
+        });
+    });
+
+blurValidation();
+
+function blurValidation() {
+    name.addEventListener('blur', () => {
+        name.classList.remove('is-invalid', 'is-valid');
+        if (name.value.trim() === '') {
+            name.classList.add('is-invalid');
+            invalidName.textContent = 'Nama Alat tidak boleh kosong';
+        } else if (!regex.test(name.value.trim())) {
+            name.classList.add('is-invalid');
+            invalidName.textContent = 'Nama Alat tidak valid';
+        } else {
+            name.classList.add('is-valid')
+            validName.textContent = 'Bagus!';
+        }
+    });
+
+    type.addEventListener('blur', () => {
+        type.classList.remove('is-invalid', 'is-valid');
+        if (type.value.trim() === '') {
+            type.classList.add('is-invalid');
+            invalidType.textContent = 'tipe Alat tidak boleh kosong';
+        } else if (!regex.test(type.value.trim())) {
+            type.classList.add('is-invalid');
+            invalidType.textContent = 'tipe Alat tidak valid';
+        } else {
+            name.classList.add('is-valid')
+            validType.textContent = 'Bagus!';
+        }
+    });
+
+    description.addEventListener('blur', () => {
+        description.classList.remove('is-invalid', 'is-valid');
+        if (description.value.trim() === '') {
+            description.classList.add('is-invalid');
+            invalidDescription.textContent = 'deskripsi Alat tidak boleh kosong';
+        } else if (!regex.test(description.value.trim())) {
+            description.classList.add('is-invalid');
+            invalidDescription.textContent = 'deskripsi Alat tidak valid';
+        } else {
+            name.classList.add('is-valid')
+            validName.textContent = 'Bagus!';
+        }
+    });
+
+    btnSimpan.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let isValid = true;
+
+        name.classList.remove('is-invalid', 'is-valid');
+        if (name.value.trim() === '') {
+            name.classList.add('is-invalid');
+            invalidName.textContent = 'Nama Alat tidak boleh kosong';
+            isValid = false;
+        } else if (!regex.test(name.value.trim())) {
+            name.classList.add('is-invalid');
+            invalidName.textContent = 'Nama Alat tidak valid';
+            isValid = false;
+        } else {
+            name.classList.add('is-valid')
+            validName.textContent = 'Bagus!';
+        }
+
+        type.classList.remove('is-invalid', 'is-valid');
+        if (type.value.trim() === '') {
+            type.classList.add('is-invalid');
+            invalidType.textContent = 'tipe Alat tidak boleh kosong';
+            isValid = false;
+        } else if (!regex.test(type.value.trim())) {
+            type.classList.add('is-invalid');
+            invalidType.textContent = 'tipe Alat tidak valid';
+            isValid = false;
+        } else {
+            name.classList.add('is-valid')
+            validType.textContent = 'Bagus!';
+        }
+
+        description.classList.remove('is-invalid', 'is-valid');
+        if (description.value.trim() === '') {
+            description.classList.add('is-invalid');
+            invalidDescription.textContent = 'deskripsi Alat tidak boleh kosong';
+            isValid = false;
+        } else if (!regex.test(description.value.trim())) {
+            description.classList.add('is-invalid');
+            invalidDescription.textContent = 'deskripsi Alat tidak valid';
+            isValid = false;
+        } else {
+            name.classList.add('is-valid')
+            validName.textContent = 'Bagus!';
+        }
+
+        if (isValid) {
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success', cancelButton: 'btn btn-danger'
+                }, buttonsStyling: false
+            });
+
+            swalWithBootstrapButtons.fire({
+                title: "Perbarui data?",
+                text: "Pastikan semua data telah diisi dengan benar!",
+                icon: "question",
+                showCancelButton: true,
+                confirmButtonText: "Ya, Perbarui",
+                cancelButtonText: "Tidak, Batal!",
+                reverseButtons: true
+            })
+                .then((result) => {
+                    if (result.isConfirmed) {
+                        axios.post(`/tools/updateTools/${id}`, {
+                            'name': name.value, 'description': description.value, 'type': type.value,
+                        })
+                            .then(response => {
+                                if (response.data.success) {
+                                    swalWithBootstrapButtons.fire({
+                                        title: "Sukses!", text: "Data berhasil diperbarui!", icon: "success"
+                                    })
+                                        .then((result) => {
+                                            if (result.isConfirmed) {
+                                                window.location.href = response.data.redirect_url;
+                                            }
+                                        });
+                                } else if (response.data.success == false) {
+                                    swalWithBootstrapButtons.fire({
+                                        title: "Gagal!", text: response.data.message, icon: "error"
+                                    });
+                                }
+                            })
+                            .catch(error => {
+                                swalWithBootstrapButtons.fire({
+                                    title: "Gagal!", text: error.message, icon: "error"
+                                });
+                            });
+                    }
+                })
+        }
+    });
+}
