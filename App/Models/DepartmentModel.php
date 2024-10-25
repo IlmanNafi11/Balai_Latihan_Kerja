d3d30428d93fb8c012ae302c5f0281d8c814f25e
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-25 09:26:46 +0700

    memperbaiki format response di model

--- App/Models/DepartmentModel.php
+++ App/Models/DepartmentModel.php
@@ -13,45 +13,80 @@ class DepartmentModel
     {
         $query = "SELECT * FROM departments";
         $stmt = $this->connection->prepare($query);
-        $stmt->execute();
-        $department = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        return $department;
+        try {
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+
     }
 
     public function getDepartmentById($id)
     {
         $query = "SELECT * FROM departments WHERE id = :id";
         $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":id", $id);
-        $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        try {
+            $stmt->bindParam(":id", $id);
+            if ($stmt->execute()) {
+                return ['success' => true, 'dataByID' => $stmt->fetch(PDO::FETCH_ASSOC)];
+            } else {
+                return ['success' => false, 'message' => 'Data tidak ditemukan'];
+            }
+        } catch (PDOException $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
     }
 
     public function createDepartment($nama, $deskripsi, $instituteID)
     {
-        $query = "INSERT INTO departments (nama, deskripsi, institusi_id) VALUES (:nama, :deskripsi, :institusi_id)";
+        $query = "INSERT INTO departments (nama, deskripsi, institute_id) VALUES (:nama, :deskripsi, :institusi_id)";
         $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":nama", $nama);
-        $stmt->bindParam(":deskripsi", $deskripsi);
-        $stmt->bindParam(":institusi_id", $instituteID);
-        return $stmt->execute();
+        try {
+            $stmt->bindParam(":nama", $nama);
+            $stmt->bindParam(":deskripsi", $deskripsi);
+            $stmt->bindParam(":institusi_id", $instituteID);
+            if ($stmt->execute()) {
+                return ['success' => true, 'message' => 'Data berhasil ditambahkan', 'redirect_url' => '/department'];
+            } else {
+                return ['success' => false, 'message' => 'Data gagal ditambahkan'];
+            }
+        } catch (PDOException $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
     }
 
     public function updateDepartment($id, $nama, $deskripsi)
     {
         $query = "UPDATE departments SET nama = :nama, deskripsi = :deskripsi WHERE id = :id";
         $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":nama", $nama);
-        $stmt->bindParam(":deskripsi", $deskripsi);
-        $stmt->bindParam(":id", $id);
-        return $stmt->execute();
+        try {
+            $stmt->bindParam(":nama", $nama);
+            $stmt->bindParam(":deskripsi", $deskripsi);
+            $stmt->bindParam(":id", $id);
+            if ($stmt->execute()) {
+                return ['success' => true, 'message' => 'Data berhasil diubah', 'redirect_url' => '/department'];
+            } else {
+                return ['success' => false, 'message' => 'Data gagal diubah'];
+            }
+        } catch (PDOException $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
     }
 
     public function deleteDepartment($id)
     {
         $query = "DELETE FROM departments WHERE id = :id";
         $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":id", $id);
-        return $stmt->execute();
+        try {
+            $stmt->bindParam(":id", $id);
+            if ($stmt->execute()) {
+                return ['success' => true, 'message' => 'Data berhasil dihapus', 'redirect_url' => '/department'];
+            } else {
+                return ['success' => false, 'message' => 'Data gagal dihapus'];
+            }
+        } catch (PDOException $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
     }
 }
