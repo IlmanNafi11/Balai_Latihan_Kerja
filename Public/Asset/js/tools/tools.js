414bed97079483cb844010a418bb8c846d944b37
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-30 22:49:01 +0700

    memperbarui implementasi request delete data tools dan merapikan kode

--- Public/Asset/js/tools/tools.js
+++ Public/Asset/js/tools/tools.js
@@ -1,51 +1,25 @@
-function deleteTools(id){
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-            confirmButton: 'btn btn-success', cancelButton: 'btn btn-danger'
-        }, buttonsStyling: false
-    });
+import {successAlert, errorAlert, questionAlert, cancelAlert} from "../helper/exceptions.js";
 
-    swalWithBootstrapButtons.fire({
-        title: "Hapus data?",
-        text: "Data tidak bisa dikembalikan setelah dihapus!",
-        icon: "question",
-        showCancelButton: true,
-        confirmButtonText: "Ya, Hapus",
-        cancelButtonText: "Tidak, Batal",
-        reverseButtons: true
-    })
-        .then((result) => {
-            if (result.isConfirmed) {
-                axios.delete(`/tools/delete/${id}`)
-                    .then(response => {
-                        if (response.data.success == true) {
-                            swalWithBootstrapButtons.fire({
-                                title: "Sukses!",
-                                text: "Data berhasil dihapus!",
-                                icon: "success"
-                            })
-                                .then((result) => {
-                                    if (result.isConfirmed) {
-                                        window.location.href = response.data.redirect_url;
-                                    }
-                                })
-                        } else if (response.data.success == false) {
-                            swalWithBootstrapButtons.fire({
-                                title: "Gagal!", text: response.data.message, icon: "error"
-                            });
-                        }
-                    })
-                    .catch(error => {
-                        swalWithBootstrapButtons.fire({
-                            title: "Gagal!",
-                            text: error.message,
-                            icon: "error"
-                        });
-                    });
-            } else {
-                swalWithBootstrapButtons.fire({
-                    title: "Dibatalkan", text: "Data Anda aman :)", icon: "error"
-                });
-            }
-        });
+window.deleteTools = deleteTools;
+
+function deleteTools(id) {
+    questionAlert("Hapus Data?", "Data tidak dapat dikembalikan setelah dihapus!", "Ya Hapus", () => {
+        axios.delete(`/tools/delete/${id}`)
+            .then(response => {
+                if (response.data.success) {
+                    const row = document.getElementById(`row-${id}`);
+                    if (row) {
+                        row.remove();
+                    }
+                    successAlert("Data Berhasil dihapus!", response.data.redirect_url, false)
+                } else {
+                    errorAlert(response.data.message);
+                }
+            })
+            .catch(error => {
+                errorAlert(error.message);
+            });
+    }, () => {
+        cancelAlert("Data anda aman :)")
+    });
 }
