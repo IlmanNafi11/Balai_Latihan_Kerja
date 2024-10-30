2a8a07def295293cb56042048cf20438f466eeba
IlmanNafi11 <ilmannafi04@gmail.com>
2024-10-30 17:43:32 +0700

    fix kode request add data buildings

--- Public/Asset/js/buildings/addBuildings.js
+++ Public/Asset/js/buildings/addBuildings.js
@@ -1,3 +1,6 @@
+import {blurValidate, onSaveValidate} from "../helper/validators.js";
+import {questionAlert, errorAlert, successAlert} from "../helper/exceptions.js";
+
 const name = document.getElementById('nama-gedung');
 const description = document.getElementById('deskripsi-gedung');
 const btnSimpan = document.getElementById('btn-simpan');
@@ -6,124 +9,34 @@ const validName = name.nextElementSibling;
 const invalidName = validName.nextElementSibling;
 const validDescription = description.nextElementSibling;
 const invalidDescription = validDescription.nextElementSibling;
-let isValid = true;
-const regexName = /^[a-zA-Z0-9 ]+$/;
+const regexName = /^[a-zA-Z0-9 .]+$/;
 const regexDecrp = /^[a-zA-Z0-9 ,.]+$/;
 
-validate();
+// Blur Validations event
+blurValidate(name, "Nama Gedung", validName, invalidName, null, regexName, 50);
+blurValidate(description, "Deskripsi Gedung", validDescription, invalidDescription, null, regexDecrp, 255);
+
 btnSimpan.addEventListener('click', (e) => {
     e.preventDefault();
-    isValid = true;
-    name.classList.remove('is-valid', 'is-invalid');
-    if (name.value.trim() === '') {
-        name.classList.add('is-invalid');
-        invalidName.textContent = 'Nama gedung tidak boleh kosong';
-        isValid = false;
-    } else if (!regexName.test(name.value.trim())) {
-        name.classList.add('is-invalid');
-        invalidName.textContent = 'Nama gedung tidak valid';
-        isValid = false;
-    } else {
-        name.classList.add('is-valid');
-        validName.textContent = 'Bagus!';
-    }
-
-    description.classList.remove('is-valid', 'is-invalid');
-    if (description.value.trim() === '') {
-        description.classList.add('is-invalid');
-        invalidDescription.textContent = 'Deskripsi gedung tidak boleh kosong';
-        isValid = false;
-    } else if (!regexDecrp.test(description.value.trim())) {
-        description.classList.add('is-invalid');
-        invalidDescription.textContent = 'Deskripsi gedung tidak valid';
-        isValid = false;
-    } else {
-        description.classList.add('is-valid');
-        validDescription.textContent = 'Bagus!';
-    }
-
+    let isValid = true;
+    isValid = onSaveValidate(name, "Nama Gedung", validName, invalidName, null, regexName, 50) && isValid;
+    isValid = onSaveValidate(description, "Deskripsi Gedung", validDescription, invalidDescription, null, regexDecrp, 255) && isValid;
     if (isValid) {
-        const swalWithBootstrapButtons = Swal.mixin({
-            customClass: {
-                confirmButton: 'btn btn-success',
-                cancelButton: 'btn btn-danger'
-            }, buttonsStyling: false
-        });
-
-        swalWithBootstrapButtons.fire({
-            title: "Simpan data?",
-            text: "Pastikan semua data telah diisi dengan benar!",
-            icon: "question",
-            showCancelButton: true,
-            confirmButtonText: "Ya, Simpan",
-            cancelButtonText: "Tidak, Batal!",
-            reverseButtons: true
-        })
-            .then((result) => {
-                if (result.isConfirmed) {
-                    axios.post('/building/addBuilding/create', {
-                        'name': name.value.trim(),
-                        'description': description.value.trim(),
-                    })
-                        .then(response => {
-                            if (response.data.success) {
-                                swalWithBootstrapButtons.fire({
-                                    title: "Sukses!",
-                                    text: "Data berhasil disimpan!",
-                                    icon: "success"
-                                })
-                                    .then((result) => {
-                                        if (result.isConfirmed) {
-                                            window.location.href = response.data.redirect_url;
-                                        }
-                                    });
-                            } else if (response.data.success == false) {
-                                swalWithBootstrapButtons.fire({
-                                    title: "Gagal!",
-                                    text: response.data.message,
-                                    icon: "error"
-                                });
-                            }
-                        })
-                        .catch(error => {
-                            swalWithBootstrapButtons.fire({
-                                title: "Gagal!",
-                                text: error.message,
-                                icon: "error"
-                            });
-                        });
-
-                }
+        questionAlert("Simpan data?", "Pastikan semua data telah diisi dengan benar!", "Ya, Simpan", () => {
+            axios.post('/building/addBuilding', {
+                'name': name.value.trim(),
+                'description': description.value.trim(),
             })
+                .then(response => {
+                    if (response.data.success){
+                        successAlert("Data berhasil disimpan!", response.data.redirect_url);
+                    } else {
+                        errorAlert(response.data.message);
+                    }
+                })
+                .catch(error => {
+                    errorAlert(`Terjadi Kesalahan: ${error.message}`);
+                });
+        });
     }
 });
-
-function validate() {
-    name.addEventListener('blur', () => {
-        name.classList.remove('is-valid', 'is-invalid');
-        if (name.value.trim() === '') {
-            name.classList.add('is-invalid');
-            invalidName.textContent = 'Nama gedung tidak boleh kosong';
-        } else if (!regexName.test(name.value.trim())) {
-            name.classList.add('is-invalid');
-            invalidName.textContent = 'Nama gedung tidak valid';
-        } else {
-            name.classList.add('is-valid');
-            validName.textContent = 'Bagus!';
-        }
-    });
-
-    description.addEventListener('blur', () => {
-        description.classList.remove('is-valid', 'is-invalid');
-        if (description.value.trim() === '') {
-            description.classList.add('is-invalid');
-            invalidDescription.textContent = 'Deskripsi gedung tidak boleh kosong';
-        } else if (!regexDecrp.test(description.value.trim())) {
-            description.classList.add('is-invalid');
-            invalidDescription.textContent = 'Deskripsi gedung tidak valid';
-        } else {
-            description.classList.add('is-valid');
-            validDescription.textContent = 'Bagus!';
-        }
-    });
-}
