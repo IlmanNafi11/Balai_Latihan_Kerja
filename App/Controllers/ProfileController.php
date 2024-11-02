0b07c8327e40e825478b8e751762deb6d2f80919
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-02 20:31:50 +0700

    menambahkan fungsi index untuk menampilkan main page profile

--- App/Controllers/ProfileController.php
+++ App/Controllers/ProfileController.php
@@ -2,5 +2,16 @@
 
 class ProfileController
 {
+    private $model;
+    public function __construct()
+    {
+        $database = new Database();
+        $db = $database->getConnection();
 
+    }
+
+    public function index()
+    {
+        require_once "../App/Views/Profiles/profile.php";
+    }
 }
