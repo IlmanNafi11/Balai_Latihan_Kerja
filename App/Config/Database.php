710e4086684f15608e72c59751cb689274d5b8ce
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-03 13:00:58 +0700

    memperbarui kode di config database menggunakan variable yang disimpan pada env

--- App/Config/Database.php
+++ App/Config/Database.php
@@ -1,11 +1,26 @@
 <?php
 class Database
 {
-    private $host = "localhost";
-    private $user = "root";
-    private $pass = "";
-    private $db = "balai_latihan_kerja";
+    private $host;
+    private $user;
+    private $pass;
+    private $db;
     public $connection;
+
+    /**
+     * @param $host
+     * @param $user
+     * @param $pass
+     * @param $db
+     */
+    public function __construct()
+    {
+        $this->host = $_ENV['DB_HOST'];
+        $this->user = $_ENV['DB_USER'];
+        $this->pass = $_ENV['DB_PASS'];
+        $this->db = $_ENV['DB_NAME'];
+    }
+
 
     public function getConnection()
     {
