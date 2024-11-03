2c8159b6abf9166bbd35972d557651696174e2f8
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-03 19:25:19 +0700

    memperbaiki kode pada login.js

--- Public/Asset/js/auth/login.js
+++ Public/Asset/js/auth/login.js
@@ -1,3 +1,6 @@
+import {blurValidate, onSaveValidate} from "../helper/validators.js";
+import {errorAlert} from "../helper/exceptions.js";
+
 const loginButton = document.getElementById('login-button');
 const validEmail = document.getElementById('valid-feedback-email');
 const invalidEmail = document.getElementById('invalid-feedback-email');
@@ -6,62 +9,30 @@ const invalidPassword = document.getElementById('invalid-feedback-password');
 let email = document.getElementById('input-email');
 let password = document.getElementById('input-password');
 
-function validate() {
+blurValidate(email, "Email", validEmail, invalidEmail, null, null, 100);
+blurValidate(password, "Password", validPassword, invalidPassword, null, null, 100);
 
-
-    email.addEventListener('blur', () => {
-        email.classList.remove('is-valid', 'is-invalid');
-        if (email.value.trim() === '') {
-            email.classList.add('is-invalid');
-            invalidEmail.textContent = 'Email tidak boleh kosong';
-            isValid = false;
-        }
-    });
-
-    password.addEventListener('blur', () => {
-        password.classList.remove('is-invalid', 'is-valid');
-        if (password.value.trim() === '') {
-            password.classList.add('is-invalid');
-            invalidPassword.textContent = 'Password tidak boleh kosong';
-            isValid = false;
-        }
-    });
-}
 
 loginButton.addEventListener('click', () => {
     let isValid = true;
-    if (email.value.trim() === '') {
-        email.classList.add('is-invalid');
-        invalidEmail.textContent = 'Email tidak boleh kosong';
-        isValid = false;
-    }
-
-    if (password.value.trim() === '') {
-        password.classList.add('is-invalid');
-        invalidPassword.textContent = 'Password tidak boleh kosong';
-        isValid = false;
-    }
-    validate();
+    isValid = onSaveValidate(email, "Email", validEmail, invalidEmail, null, null, 100) && isValid;
+    isValid = onSaveValidate(password, "Password", validPassword, invalidPassword, null, null, 100) && isValid;
 
     if (isValid) {
-        // Logika login
-        // Mengirim request login menggunakan Axios
         axios.post('/login', {
             email: email.value,
             password: password.value
         })
             .then(response => {
                 const data = response.data;
-                console.log(data);
                 if (data.status === 'success') {
                     window.location.href = data.redirect_url;
                 } else {
-                    alert(data.message); // Tampilkan pesan error
+                    errorAlert(data.message);
                 }
             })
             .catch(error => {
-                console.error(error);
-                alert("Terjadi kesalahan saat mencoba login. Silakan coba lagi.");
+                errorAlert(error.message);
             });
     }
 });
