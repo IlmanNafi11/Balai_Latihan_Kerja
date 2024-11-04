66e982a61001fd346da57a64bd42fec188d618af
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-04 23:02:20 +0700

    menampilkan data user ke profile

--- App/Controllers/ProfileController.php
+++ App/Controllers/ProfileController.php
@@ -14,11 +14,28 @@ class ProfileController
 
     public function index()
     {
+        $id = $_SESSION['userID'];
+        $users = $this->model->getUsersById($id);
+        $profile = "/Public/" . htmlspecialchars($users['users']['profile_picture']);
         require_once "../App/Views/Profiles/profile.php";
     }
 
     public function getUsersById($id)
     {
         echo json_encode($this->model->getUsersById($id));
+    }
+
+    public function updateProfile($id)
+    {
+        $data = json_decode(file_get_contents("php://input"), true);
+        $name = $data['name'];
+        $no_tlp = $data['phone'];;
+        $address = $data['address'];
+        if (!empty($name) && !empty($no_tlp) && !empty($address)) {
+            echo json_encode($this->model->updateAdmin($id, $name, $no_tlp, $address));
+        } else {
+            echo json_encode(['status' => false, 'message' => 'Data tidak lengkap']);
+        }
+
     }
 }
