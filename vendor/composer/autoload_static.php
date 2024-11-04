cf768ede44a7cbe48c3e116f254323ebe036599e
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-04 17:01:20 +0700

    install library firebase/php-jwt

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -27,6 +27,10 @@ class ComposerStaticInitd45b0c8653cf2d7fe512dae6f2e66523
         array (
             'GrahamCampbell\\ResultType\\' => 26,
         ),
+        'F' => 
+        array (
+            'Firebase\\JWT\\' => 13,
+        ),
         'D' => 
         array (
             'Dotenv\\' => 7,
@@ -53,6 +57,10 @@ class ComposerStaticInitd45b0c8653cf2d7fe512dae6f2e66523
         'GrahamCampbell\\ResultType\\' => 
         array (
             0 => __DIR__ . '/..' . '/graham-campbell/result-type/src',
+        ),
+        'Firebase\\JWT\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
         ),
         'Dotenv\\' => 
         array (
