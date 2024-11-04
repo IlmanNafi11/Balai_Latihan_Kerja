75ecde1dda98b4b0d4306d9e78e1a39a01d74815
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-04 19:28:56 +0700

    memperbarui implementasi autenticate di middleware

--- App/Middleware/AuthMiddleware.php
+++ App/Middleware/AuthMiddleware.php
@@ -1,26 +1,31 @@
 <?php
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 function authenticate()
 {
-    $service = new ServiceToken();
+    $secretKey = $_ENV['JWT_SECRET'];
     $headers = apache_request_headers();
     $authHeader = $headers['Authorization'];
 
     if (!$authHeader) {
+        http_response_code(401);
         echo json_encode(['status' => 'error', 'message' => 'Token tidak ditemukan']);
-        http_response_code(401);
-        exit;
+        return false;
     }
 
-    $token = str_replace('Bearer ', '', $authHeader);
-
-    $payload = $service->verifyToken($token);
-
-    if (!$payload) {
-        echo json_encode(['status' => 'error', 'message' => 'Token tidak valid atau kadaluarsa']);
-        http_response_code(401);
-        exit;
+    if (preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
+        $jwt = $matches[1];
+        try {
+            $decoded = JWT::decode($jwt, new Key($secretKey, 'HS256'));
+            $userId = $decoded->users->id;
+            return $userId;
+        } catch (Exception $e) {
+            http_response_code(401);
+            echo json_encode(['status' => 'error', 'message' => "Token tidak valid: " . $e->getMessage()]);
+            return false;
+        }
     }
-
-    return $payload;
 }
 
