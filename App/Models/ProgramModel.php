b173f5663ca369bd78ffe7454f6012de199fe95b
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-04 16:45:08 +0700

    fix kesalahan get data dan menambahkan metode untuk get data programs berdasarkan id departments

--- App/Models/ProgramModel.php
+++ App/Models/ProgramModel.php
@@ -9,7 +9,7 @@ class ProgramModel
         $this->connection = $db;
     }
 
-    public function getAllProgram()
+    public function getAllProgram() // Notes
     {
         $query = "SELECT programs.*, buildings.nama AS building_name, instructors.nama AS instructor_name, departments.nama AS department_name FROM programs JOIN buildings ON programs.building_id = buildings.id JOIN instructors ON programs.instructor_id = instructors.id JOIN departments ON programs.department_id = departments.id ORDER BY programs.id ASC";
         $stmt = $this->connection->prepare($query);
@@ -25,7 +25,7 @@ class ProgramModel
         }
     }
 
-    public function getProgramById($id)
+    public function getProgramById($id) // Notes
     {
         $query = "SELECT programs.*, buildings.nama AS building_name, instructors.nama AS instructor_name, departments.nama AS department_name FROM programs JOIN buildings ON programs.building_id = buildings.id JOIN instructors ON programs.instructor_id = instructors.id JOIN departments ON programs.department_id = departments.id WHERE programs.id = :id ORDER BY programs.id ASC";
         $stmt = $this->connection->prepare($query);
@@ -43,9 +43,27 @@ class ProgramModel
 
     public function getProgramsByDepartment($id)
     {
-        $query = "SELECT programs.*, buildings.nama AS building_name, departments.nama AS department_name, instructors.nama AS instructor_name, instructors.no_tlp AS instructors_contact, instructors.alamat AS instructors_address FROM programs LEFT JOIN buildings ON programs.building_id = buildings.id LEFT JOIN departments ON programs.department_id = departments.id LEFT JOIN instructors ON programs.instructor_id = instructors.id WHERE departments.id = :department_id";
+        $query  = "SELECT id, nama, deskripsi FROM programs WHERE department_id = :id";
+        $stmt   = $this->connection->prepare($query);
+        try {
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (empty($data)) {
+                return ['success' => true, 'isEmpty' => true, 'message' => 'Data Tidak ditemukan'];
+            } else {
+                return ['success' => true, 'isEmpty' => false, 'programs' => $data];
+            }
+        } catch (PDOException $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+    }
+
+    public function getProgramsById($id)
+    {
+        $query = "SELECT programs.*, buildings.nama AS building_name, departments.nama AS department_name, instructors.nama AS instructor_name, instructors.no_tlp AS instructors_contact, instructors.alamat AS instructors_address FROM programs LEFT JOIN buildings ON programs.building_id = buildings.id LEFT JOIN departments ON programs.department_id = departments.id LEFT JOIN instructors ON programs.instructor_id = instructors.id WHERE programs.id = :id";
         $stmt = $this->connection->prepare($query);
-        $stmt->bindParam(":department_id", $id);
+        $stmt->bindParam(":id", $id);
         try {
             $stmt->execute();
             $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
