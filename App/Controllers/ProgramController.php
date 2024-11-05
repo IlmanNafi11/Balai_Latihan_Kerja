660960b49d29e280787dd33ce93e21f24c076926
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-06 06:09:51 +0700

    membuat metode untuk handle request insert data program

--- App/Controllers/ProgramController.php
+++ App/Controllers/ProgramController.php
@@ -43,4 +43,39 @@ class ProgramController
         echo json_encode($this->model->getProgramsByDepartment($id));
     }
 
+    public function createPrograms()
+    {
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (empty($data)) {
+            echo json_encode(['success' => false, 'message' => 'Data Kosong']);
+            return;
+        }
+
+        $programs = [
+            'nama' => $data['name'],
+            'status_pendaftaran' => $data['status_register'],
+            'tgl_mulai_pendaftaran' => $data['start_date'],
+            'tgl_akhir_pendaftaran' => $data['end_date'],
+            'standar' => $data['standar'],
+            'jml_peserta' => $data['participant'],
+            'deskripsi' => $data['description'],
+            'instructor_id' => $data['instructor_id'],
+            'building_id' => $data['building_id'],
+            'department_id' => $data['department_id'],
+            'requirements' => $data['requirements']
+        ];
+
+        if (!empty($programs)) {
+            try {
+                $result = $this->model->createProgram($programs);
+                echo json_encode($result);
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+                echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+            }
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Data Kosong']);
+        }
+
+    }
 }
