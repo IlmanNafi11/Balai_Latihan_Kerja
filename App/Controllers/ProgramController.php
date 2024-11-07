b6325d5004480d9661692dd8903d7409325fd2b2
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-07 13:24:41 +0700

    membuat metode untuk handle request update data program.

--- App/Controllers/ProgramController.php
+++ App/Controllers/ProgramController.php
@@ -4,12 +4,17 @@ require_once '../App/Models/ProgramModel.php';
 class ProgramController
 {
     private $model;
+    private $db;
+
+    private $updateResult = true;
+    private $insertResult = true;
+    private $deleteResult = true;
 
     public function __construct()
     {
         $database = new Database();
-        $db = $database->getConnection();
-        $this->model = new ProgramModel($db);
+        $this->db = $database->getConnection();
+        $this->model = new ProgramModel($this->db);
     }
 
     public function index()
@@ -31,6 +36,26 @@ class ProgramController
     public function getAllPrograms()
     {
         return json_encode($this->model->getAllProgram());
+    }
+
+    public function getProgramsByIdForUpdate($id)
+    {
+        require_once '../App/Models/RequirementsModel.php';
+        $programs = $this->model->getProgramByIdForUpdate($id);
+        if ($programs['success'] && !$programs['isEmpty']) {
+            $requirementsModel = new RequirementsModel($this->db);
+            $requirements = $requirementsModel->getRequirementsByProgram($id);
+            if ($requirements['success'] && !$requirements['isEmpty']) {
+                $programs['requirements'] = $requirements['requirements'];
+                echo json_encode($programs);
+            } else {
+                http_response_code(404);
+                echo $requirements;
+            }
+        } else {
+            http_response_code(404);
+            echo $programs;
+        }
     }
 
     public function getProgramsById($id)
@@ -77,5 +102,70 @@ class ProgramController
             echo json_encode(['success' => false, 'message' => 'Data Kosong']);
         }
 
+    }
+
+    public function updatePrograms($id)
+    {
+        require_once '../App/Controllers/RequirementsController.php';
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (empty($data)) {
+            echo json_encode(['success' => false, 'message' => 'Data Kosong']);
+            return;
+        }
+
+        $programs = [
+            'id' => $id,
+            'nama' => $data['name'],
+            'status_pendaftaran' => $data['status_register'],
+            'tgl_mulai_pendaftaran' => $data['start_date'],
+            'tgl_akhir_pendaftaran' => $data['end_date'],
+            'standar' => $data['standar'],
+            'jml_peserta' => $data['participant'],
+            'deskripsi' => $data['description'],
+            'instructor_id' => $data['instructor_id'],
+            'building_id' => $data['building_id'],
+            'department_id' => $data['department_id'],
+        ];
+
+        if (!empty($programs)) {
+            try {
+                $programResult = $this->model->updatePrograms($programs);
+                if ($programResult['success']) {
+                    if (!empty($data['requirements'])) {
+                        $requirementsController = new RequirementsController();
+                        $result = $requirementsController->updateRequirements($data['requirements']);
+                        if (!$result['success']) {
+                            return $this->updateResult = false;
+                        }
+                    }
+
+                    if (!empty($data['new_requirements'])) {
+                        $requirementsController = new RequirementsController();
+                        $result = $requirementsController->addNewRequirements($data['new_requirements'], $id);
+                        if (!$result['success']) {
+                            return $this->insertResult = false;
+                        }
+                    }
+
+                    if (!empty($data['delete_requirements'])) {
+                        $requirementsController = new RequirementsController();
+                        $result = $requirementsController->deleteRequirements($data['delete_requirements']);
+                        if (!$result['success']) {
+                            return $this->deleteResult = false;
+                        }
+                    }
+                    if ($this->updateResult && $this->insertResult && $this->deleteResult) {
+                        echo json_encode($programResult);
+                    }
+                } else {
+                    echo json_encode(['success' => false, 'message' => $programResult['message']]);
+                }
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+                echo json_encode(['success' => false, 'message' => $e->getMessage()]);
+            }
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Data Kosong']);
+        }
     }
 }
