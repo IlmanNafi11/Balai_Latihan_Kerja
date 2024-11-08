d8d3e3af54a7a93bb6a92efaf1af5da7ee5dbfdc
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-08 19:20:20 +0700

    koding event input dan keydown untuk form otp verify

--- Public/Asset/js/script.js
+++ Public/Asset/js/script.js
@@ -1,8 +1,29 @@
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const sliderNavigation = document.getElementById("slider-navigation");
 
-hamburgerMenu.addEventListener("click", function () {
-  // Toggle class active pada hamburger dan slider
-  hamburgerMenu.classList.toggle("active");
-  sliderNavigation.classList.toggle("active");
-});
+if (hamburgerMenu) {
+  hamburgerMenu.addEventListener("click", function () {
+    // Toggle class active pada hamburger dan slider
+    hamburgerMenu.classList.toggle("active");
+    sliderNavigation.classList.toggle("active");
+  });
+}
+
+
+const inputs = document.querySelectorAll('.otp-field input');
+if (inputs) {
+  inputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+      if (input.value.length === 1 && index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    });
+    input.addEventListener('keydown', (e) => {
+      if (e.key === 'Backspace' && index > 0 && input.value === '') {
+        inputs[index - 1].focus();
+      }
+    });
+  });
+}
+
+
