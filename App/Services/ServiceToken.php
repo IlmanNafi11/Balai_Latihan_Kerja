a05fcd7b8aaf8919c3b42d1bdc52067eeb5389fe
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-11 01:37:58 +0700

    memperbarui implementasi ServiceToken.php

--- App/Services/ServiceToken.php
+++ App/Services/ServiceToken.php
@@ -11,22 +11,8 @@ class ServiceToken
         $this->secret = $_ENV['JWT_SECRET'];
     }
 
-    public function createToken($userid, $email, $role, $username)
+    public function createToken($payload)
     {
-        $issuedAt = time();
-        $expired = $issuedAt + 3600;
-
-        $payload = [
-            'iat' => $issuedAt,
-            'exp' => $expired,
-            "users" => [
-                'id' => $userid,
-                'email' => $email,
-                'role' => $role,
-                'username' => $username
-            ]
-        ];
-
         $token = JWT::encode($payload, $this->secret, 'HS256');
         return $token;
     }
