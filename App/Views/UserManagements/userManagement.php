1b023f468b437727377af62eee0a3ff691f8839c
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-12 01:12:44 +0700

    import library

--- App/Views/UserManagements/userManagement.php
+++ App/Views/UserManagements/userManagement.php
@@ -84,6 +84,15 @@
     <!-- Custom Js-->
     <script src="/Asset/js/script.js"></script>
 
+    <!-- AXIOS -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+    <!-- SweetAlert -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- Logic Handler -->
+    <script type="module" src="/Asset/js/usersManagement/usersManagement.js"></script>
+
 </body>
 
 </html>
