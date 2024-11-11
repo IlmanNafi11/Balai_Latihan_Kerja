8c72f38c032e78ae6f86f268eea6f4fe0728064d
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-11 22:42:48 +0700

    menambahkan classlist pada element input otp dan import file js

--- App/Views/Auth/OTPAuth.php
+++ App/Views/Auth/OTPAuth.php
@@ -11,8 +11,8 @@
 
     <!-- Google Font -->
     <link
-        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-        rel="stylesheet">
+            href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+            rel="stylesheet">
 
     <link rel="stylesheet" href="/Asset/css/style.css">
     <title>Lupa Kata Sandi | BLK NGANJUK</title>
@@ -35,29 +35,27 @@
             <!-- Title -->
             <div class="title-container mb-4 d-flex flex-column row-gap-3">
                 <h3 class="m-0">Verifikasi Kode OTP</h3>
-                <small class="subtitle-container">Masukan kode otentikasi telah dikirim ke email Anda.</small>
+                <small class="subtitle-container">Masukan kode OTP yang telah dikirim ke email Anda.</small>
             </div>
             <form class="form-input-otp d-flex flex-column h-100 row-gap-4">
                 <div class="input-field-container w-100 p-0 m-0 d-flex flex-column row-gap-3">
                     <div class="otp-field w-auto d-flex justify-content-center column gap-2">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <div class="valid-feedback"></div>
-                        <div class="invalid-feedback"></div>
+                        <input type="text" maxlength="1" class="otp-input">
+                        <input type="text" maxlength="1" class="otp-input">
+                        <input type="text" maxlength="1" class="otp-input">
+                        <input type="text" maxlength="1" class="otp-input">
+                        <input type="text" maxlength="1" class="otp-input">
+                        <input type="text" maxlength="1" class="otp-input">
                     </div>
                     <div class="resend-container">
                         <small class="subtitle-resend-otp small-subtitle resend">Tidak menerima kode?</small>
-                        <a href="#" class="otp-reSend-button text-decoration-none resend">Kirim Ulang</a>
+                        <span class="otp-reSend-button text-decoration-none resend" id="resendOtp">Kirim Ulang</span>
                     </div>
                 </div>
                 <div class="verify-otp-btn-container w-100 d-flex justify-content-center">
-                    <a href="/password-reset/new">
-                        <button type="button" class="btn btn-verify btn-credensial" name="verify" id="btn-verify-otp">Selanjutnya</button>
-                    </a>
+                    <button type="button" class="btn btn-verify btn-credensial" name="verify" id="btn-verify-otp">
+                        Selanjutnya
+                    </button>
                 </div>
             </form>
         </div>
@@ -76,6 +74,8 @@
 
 <!-- SweetAlert -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script type="module" src="/Asset/js/auth/resetPassword.js"></script>
 
 </body>
 </html>
