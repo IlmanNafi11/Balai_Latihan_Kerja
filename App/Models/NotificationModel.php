712e1ecdcf0bd5ebbf28fcbd696a9661deecc368
IlmanNafi11 <ilmannafi04@gmail.com>
2024-11-24 09:30:20 +0700

    menyediakan model untuk update data is_read, is_deleted, dan get notification by user id

--- App/Models/NotificationModel.php
+++ App/Models/NotificationModel.php
@@ -17,14 +17,11 @@ class NotificationModel
             $stmt->execute();
             $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
             if (empty($data)) {
-                http_response_code(204);
                 return ['success' => true, 'isEmpty' => true, 'message' => "Data Kosong", 'notifications' => []];
             } else {
-                http_response_code(200);
                 return ['success' => true, 'isEmpty' => false, 'notifications' => $data];
             }
         } catch (PDOException $e) {
-            http_response_code(500);
             return ['success' => false, 'message' => $e->getMessage()];
         }
     }
@@ -52,11 +49,11 @@ class NotificationModel
 
     public function createNotification($message)
     {
-        $query = "INSERT INTO notifications (pesan, tipe) VALUES(:message, :tipe)";
+        $query = "INSERT INTO notifications (pesan, target) VALUES(:message, :target)";
         $stmt = $this->connection->prepare($query);
         try {
             $stmt->bindParam(':message', $message);
-            $stmt->bindValue(':tipe', 'publik');
+            $stmt->bindValue(':target', 'Publik');
             $stmt->execute();
             http_response_code(200);
             return ['success' => true, 'message' => 'Notifikasi berhasil ditambahkan', 'redirect' => '/notification'];
@@ -97,6 +94,77 @@ class NotificationModel
                 http_response_code(200);
                 return ['success' => true, 'isEmpty' => false, 'notifications' => $data];
             }
+        } catch (PDOException $e) {
+            http_response_code(500);
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+    }
+
+    public function getUpdateNotification($lastCeck)
+    {
+        $query = "SELECT * FROM notifications WHERE created_at > :lastCheck";
+        $stmt = $this->connection->prepare($query);
+        try {
+            $stmt->bindParam(':lastCheck', $lastCeck);
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (empty($data)) {
+                return ['success' => true, 'isEmpty' => true, 'message' => 'Data Kosong', 'notifications' => []];
+            } else {
+                $data[0]['is_read'] = 0;
+                return ['success' => true, 'isEmpty' => false, 'notifications' => $data];
+            }
+
+        } catch (PDOException $e) {
+            return [
+                'success' => false,
+                'message' => $e->getMessage()
+            ];
+        }
+    }
+
+    public function getNotificationByUserId($id)
+    {
+        $query = "SELECT n.id AS notification_id, n.pesan, n.created_at AS notification_created_at, un.id AS id, un.is_read, un.is_deleted FROM notifications n INNER JOIN user_notifications un ON n.id = un.notification_id WHERE un.user_id = :user_id AND un.is_deleted = 0 ORDER BY n.created_at DESC";
+        $stmt = $this->connection->prepare($query);
+        try {
+            $stmt->bindParam(':user_id', $id);
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (empty($data)) {
+                return ['success' => true, 'isEmpty' => true, 'message' => 'Data Kosong', 'notifications' => []];
+            } else {
+                return ['success' => true, 'isEmpty' => false, 'notifications' => $data];
+            }
+        } catch (PDOException $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+    }
+
+    public function updateIsRead($id)
+    {
+        $query = "UPDATE user_notifications SET is_read = TRUE WHERE id = :id";
+        $stmt = $this->connection->prepare($query);
+        try {
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            http_response_code(200);
+            return ['success' => true, 'message' => 'Notifikasi berhasil diperbarui'];
+        } catch (PDOException $e) {
+            http_response_code(500);
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+    }
+
+    public function updateIsDeleted($id)
+    {
+        $query = "UPDATE user_notifications SET is_deleted = 1 WHERE id = :id";
+        $stmt = $this->connection->prepare($query);
+        try {
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            http_response_code(200);
+            return ['success' => true, 'message' => 'Notifikasi berhasil dihapus'];
         } catch (PDOException $e) {
             http_response_code(500);
             return ['success' => false, 'message' => $e->getMessage()];
