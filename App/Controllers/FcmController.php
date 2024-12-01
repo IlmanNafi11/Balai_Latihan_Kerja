d2d03d470f8041c8be38d1d8f0c8abee3f92d561
IlmanNafi11 <ilmannafi04@gmail.com>
2024-12-02 03:35:13 +0700

    fix import path menggunakan path domain server

--- App/Controllers/FcmController.php
+++ App/Controllers/FcmController.php
@@ -1,11 +1,11 @@
 <?php
-require_once '../../vendor/autoload.php';
-require_once '../Models/NotificationModel.php';
-require_once '../Config/Database.php';
+require_once '/home/u137138991/domains/pelatihanku.pbltifnganjuk.com/public_html/vendor/autoload.php';
+require_once '/home/u137138991/domains/pelatihanku.pbltifnganjuk.com/public_html/App/Models/NotificationModel.php';
+require_once '/home/u137138991/domains/pelatihanku.pbltifnganjuk.com/public_html/App/Config/Database.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable('../../');
+$dotenv = Dotenv::createImmutable('/home/u137138991/domains/pelatihanku.pbltifnganjuk.com/public_html/');
 $dotenv->load();
 
 use Google\Client;
